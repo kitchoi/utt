4eb135144f794e2556e4d69d182b396f25e0f5af
Jason Stewart <support@eggplantsd.com>
2017-04-27 02:10:16 -0500

    TASK AUTOCOMPLETION + MISC
    * PULLED LATEST argcomplete FROM GITHUB FOR append_space= KEYWORD ARG IN
    argcomplete.autocomplete()
    * FILENAME / DATA-DIR FUNCTIONS TO util.py FOR ACCESSIBILITY OUTSIDE OF
    main
    * CUSTOM AUTOCOMPLETER FOR cmd_add.py FOR POPULATING FROM TASK HISTORY
    * 'DESCRIPTION' ATTRIBUTE FOR EACH cmd_ MODULE FOR ENHANCED argparse
    HELP ON SUBCOMMANDS
    * CONFIGURED LOGGING IN main() TO CAPTURE ANY AUTOCOMPLETE PROBLEMS
    (argcomplete CAPTURES STDERR)
    * CHECK FOR DEBIAN DEFAULT EDITOR /usr/bin/editor WHEN $VISUAL AND
    $EDITOR ARE NOT SET BEFORE FALLING BACK TO vi
    * ADDED STRING 'PYTHON_ARGCOMPLETE_OK' TO FIRST 1024 BYTES OF MAIN
    SCRIPT IN bin/ TO BE USED WITH GLOBAL ARGCOMPLETE IF ENABLED IN
    /etc/profile

--- utt/cmd_edit.py
+++ utt/cmd_edit.py
@@ -1,7 +1,8 @@
 import os
 import subprocess
 
-NAME = 'edit'
+NAME        = 'edit'
+DESCRIPTION = 'Edit task log using your system\'s default editor'
 
 
 def add_args(parser):
@@ -9,11 +10,33 @@ def add_args(parser):
 
 
 def execute(args):
-    _run_editor(_editor(), args.data_filename)
+
+    _run_editor(path_default_editor(), args.data_filename)
 
 
-def _editor():
-    return (os.environ.get("VISUAL") or os.environ.get("EDITOR", "vi"))
+def checkEnvPath(envVar):
+
+   szPath = os.environ.get(envVar, '')
+
+   if( len(szPath) > 0 ):
+      if( os.path.exists(szPath) ):
+         return szPath
+
+   return False
+
+
+def path_default_editor():
+
+    szEditorDefault = '/usr/bin/editor'
+
+    if( not os.path.exists(szEditorDefault) ):
+        szEditorDefault = 'vi'
+
+    return (
+        checkEnvPath("VISUAL") or
+        checkEnvPath("EDITOR") or
+        szEditorDefault
+    )
 
 
 def _run_editor(editor, data_filename):
